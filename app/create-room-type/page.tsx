f55571ecb01e65aeb7644ff0d9cf29e2da21f89d
GamageThanuja <w1871525@my.westminster.ac.uk>
2025-11-07 14:53:47 +0530

    test

--- app/create-room-type/page.tsx
+++ app/create-room-type/page.tsx
@@ -1,5 +1,6 @@
 import RoomTypesMultiStep from "@/components/room-types-multi-step";
 
 export default function RoomTypesMultiStepPage() {
+  // this page is used to create room types with a multi-step form
   return <RoomTypesMultiStep />;
 }
